043c0b1d614e083564da69dfe027261c397b7c22
George Wu <georgew5656@berkeley.edu>
2022-09-17 16:18:47 -0400

    saving work

---
+++ src/Blog.tsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+interface BlogRowProps {
+    date: string
+    path: string
+}
+
+
+function BlogRow(props: BlogRowProps) {
+    return <><span>{props.date}</span><span><Link to={"/" + props.path}>{props.path}</Link></span></>;
+  }
+
+export function Blog() {
+    return <div>
+        {BlogRow({date: "September 17, 2022", path: "portugal"})}
+    </div>
+}
